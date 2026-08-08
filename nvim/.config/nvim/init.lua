c97e58f913a7eddb3c5745dce2c414cc2765caa5
shen-nm <dev.github@shenmilan.com>
2026-08-08 12:54:07 +0300

    fix: color schemes no longer interfering

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -306,7 +306,6 @@ do
   -- Load the colorscheme here.
   -- Like many other themes, this one has different styles, and you could load
   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  vim.cmd.colorscheme 'tokyonight-night'
 
   -- Highlight todo, notes, etc in comments
   vim.pack.add { gh 'folke/todo-comments.nvim' }
